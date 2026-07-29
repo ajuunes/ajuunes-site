b714f496a2723db6b1a606d93c0d44b929397ec0
Agamemnon Juunes <agamemnon.juunes@gmail.com>
2026-07-28 22:48:34 -0400

    Add new messages to marqueeMessages array

--- script.js
+++ script.js
@@ -308,6 +308,12 @@ function getRandomColor() {
 const marqueeMessages = [
     "📧 contact@agamemnonjuunes.linlk 📧",
     "🐈 all new derpy cats! coming soon! 😾",
+    "🔜 all new Cooking School FAQ is in the works - Soups On!🍲",
+    "🖼️ come visit me and schwwaaa in minneapolis, mn at VideoSync 2026 sept 26-27 - more at videosync.xyz/",
+    "🌐 dumbterm - the web terminal you didn't know you needed! 💻",
+    "📺 Videobeaux - it's your best friend! Now with a GUI! 📺",
+    "⏯️ The VideoSampler10 is now opertional! ✅",
+    "🫶 schwwaaa - 𝒮𝒽𝒶𝓅𝒾𝓃𝑔 𝓉𝑜𝓂𝑜𝓇𝓇𝑜𝓌 ... 𝓉𝑜𝒹𝒶𝓎 塩カ畏 🌈™"
 ];
 
 const marqueeContainer = document.getElementById("marquee-container");
